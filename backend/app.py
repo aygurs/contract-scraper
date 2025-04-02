b47b9939ab27d4b5aa1be1d364884d0de117d852
Aydin Gursoy <aydin_gurs@hotmail.com>
2025-04-03 00:41:37 +0100

    Create Flask file, update scraper to scrape from contract page and not search results

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,36 @@
 #https://flask.palletsprojects.com/en/stable/quickstart/#a-minimal-application
+#Use Flask to allow backend and frontend to interact
+from flask import Flask, request, jsonify
+#Import scraper functions from scraper.py
+from scraper import scrape_page, save_to_csv, save_to_excel
+
+#Create instance of class
+app = Flask(__name__)
+
+#Declare what route will trigger function
+#Make Flask only respond to GET requests
+@app.route("/scrape", methods=["GET"])
+def scrape_data():
+    '''Run the scraper and return a summary as JSON'''
+
+    # Get URL from query parameters
+    url = request.args.get("url")
+    if not url:
+        return jsonify({"error": "No URL provided"}), 400
+
+    contract = scrape_page(url)
+    #Wrap into list to be saved
+    data = [contract]
+    save_to_csv(data)
+    save_to_excel(data)
+
+    #Return response in JSON format using jsonify
+    return jsonify({
+        "message": "Scraping completed successfully",
+        "data_found": len(contract)
+    })
+
+#Only run when script is run directly, not imported
+if __name__ == "__main__":
+    #Debug true temporarily for dev testing
+    app.run(debug=True)
--- backend/scraper.py
+++ backend/scraper.py
@@ -10,94 +10,80 @@ from bs4 import BeautifulSoup
 #https://pandas.pydata.org/docs/user_guide/index.html#user-guide
 import pandas as pd
 
-#Parameters we need to add to HTTP request (key:value pairs)
-#Can change page by using key:value pairs in parameters
-#Can add further filters down the line
-PARAMS = {
-    "awardNoticeFilter": "Yes",
-    "page": 1
-}
-
 #Scraper function
-#Default page number to 1 if no page number is given
-def scrape_page(page=1):
-    '''Scrape a single page of the awarded contracts results'''
-
-    #Update page in params to page requested
-    PARAMS["page"] = page
+def scrape_page(url):
+    '''Scrape the awarded contract page for metadata'''
 
     #Send GET request to desired webpage using requests
-    response = requests.get("https://www.contractsfinder.service.gov.uk/Search/Results", params=PARAMS)
+    response = requests.get(url)
 
     #Create a 'soup' object of the HTML page requested
     #.text is a property from the requests object
     #Using lxml as a parser for faster response times
     soup = BeautifulSoup(response.text, "lxml")
 
-    #Create list to hold dictionaries of data
-    contracts = []
+    #Create dictionary to hold contract data
+    #Add URL to contract
+    contract = {"Contract URL": url}
 
-    #Find all awarded contracts on page and assign them to the variable
-    #Will be a list of all found search results separated by commas
-    #Gov website uses class search_result in each result div
-    search_results = soup.find_all("div", class_="search-result")
+    #Find title
+    #Use find method of BeautifulSoup to extract information
+    title = soup.find("h1", class_="govuk-heading-l break-word")
+    #Use get_text method to strip any HTML + whitespace and extract pure text
+    #Add data to Title if title is found, or No title if not
+    contract["Title"] = title.get_text(strip=True) if title else "No title"
 
-    #Loop through each found contract to extract the data from them
-    for result in search_results:
-        #Title is held as a link on Gov website, extract title and link
-        #Use find method of BeautifulSoup to extract information
-        #Also use get_text method to strip any HTML + whitespace and extract pure text
-        title_tag = result.find("a", class_="search-result-header__title")
-        #Handle case of no title
-        if title_tag:
-            title = title_tag.get_text(strip=True)
-        else:
-            title = "No title"
+    #Find metadata in h4 tags
+    h4_tags = soup.find_all("h4")
+    for tag in h4_tags:
+        #Collect subtitle and strip all HTML
+        subtitle = tag.get_text(strip=True)
 
-        #Extract link from result title
-        raw_link = result.find("a")
-        #If there is no link, the url is then just None, instead of returning a link with None at the end
-        if raw_link and raw_link.get("href"):
-            result_link = "https://www.contractsfinder.service.gov.uk" + raw_link.get("href")
-        else:
-            result_link = None
+        #Collect industry data
+        #ADD OPTION FOR WHEN THERE IS MORE THAN 1 INDUSTRY-----------
+        if subtitle == "Industry":
+            #Target the ul after the subtitle (Unique to industry only)
+            ul = tag.find_next_sibling("ul")
+            if ul:
+                #If there is a ul, find the li element
+                li = ul.find("li")
+                if li:
+                    #If there is a li element, get the text from it and strip HTML
+                    contract["Industry"] = li.get_text(strip=True)
 
-        #Start of the dictionary of this specific contract
-        contract = {
-            "Title": title,
-            "URL": result_link
-        }
-
-        #Extract remaining metadata from the entries and add to dictionary
-        entries = result.find_all("div", class_="search-result-entry")
-        for entry in entries:
-            #Finds the part of the data in bold
-            key_value = entry.find("strong")
-            if key_value:
-                key = key_value.get_text(strip=True)
-                #Handles case where there is no more text after a bold section
-                if key_value.next_sibling:
-                    value = key_value.next_sibling.strip()
-                else:
-                    value = None
-                #Add data to contracts list
-                contract[key] = value
+        #Collect location of contract
+        elif subtitle == "Location of contract":
+            #Target the p element after the subtitle
+            p = tag.find_next_sibling("p")
+            #Add location to contract data
+            contract["Location of contract"] = p.get_text(strip=True) if p else "Unknown"
         
-        #Add dictionary to main contracts list
-        contracts.append(contract)
-    
-    #Return the completed contracts list
-    return contracts
+        #Collect value of contract
+        elif subtitle == "Value of contract":
+            #Target the p element after the subtitle
+            p = tag.find_next_sibling("p")
+            #Add location to contract data
+            contract["Value"] = p.get_text(strip=True) if p else "Unknown"
+
+        #Collect Published date
+        elif subtitle == "Published date":
+            #Target the p element after the subtitle
+            p = tag.find_next_sibling("p")
+            #Add location to contract data
+            contract["Published Date"] = p.get_text(strip=True) if p else "Unknown"
+
+            
+    return contract
 
 #Save function to csv file
 def save_to_csv(data, filename="contracts.csv"):
     '''Saves scraped contract data as a csv file'''
     
     #Creates a data frame using pandas (pd) and puts data into a table structure
-    contracts_dataframe = pd.DataFrame(data)
+    contract_dataframe = pd.DataFrame(data)
 
     #Convert to csv file, don't include index numbers in file
-    contracts_dataframe.to_csv(filename, index=False)
+    contract_dataframe.to_csv(filename, index=False)
 
     #Prints confirmation to console
     print(f"Saved {len(data)} contracts to {filename}")
@@ -107,11 +93,11 @@ def save_to_excel(data, filename="contracts.xlsx"):
     '''Saves scraped contract data as a excel file'''
     
     #Creates a data frame using pandas (pd) and puts data into a table structure
-    contracts_dataframe = pd.DataFrame(data)
+    contract_dataframe = pd.DataFrame(data)
 
     #Convert to csv file, don't include index numbers in file
     #Pandas uses openpyxl library to create excel file
-    contracts_dataframe.to_excel(filename, index=False)
+    contract_dataframe.to_excel(filename, index=False)
 
     #Prints confirmation to console
     print(f"Saved {len(data)} contracts to {filename}")
